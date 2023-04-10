6b69b0eb829e00611efb9cf426ecde78fcc685e8
Karel Rappo <karelrappo@gmail.com>
2023-04-10 15:09:49 +0200

    Prefect flows

--- week2/flows/parameterized_flow.py
+++ week2/flows/parameterized_flow.py
@@ -2,9 +2,10 @@ from pathlib import Path
 import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
+from prefect.tasks import task_input_hash
+from datetime import timedelta
 
-
-@task(retries=3)
+@task(retries=3,cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Read data from web into pandas DataFrame"""
 
@@ -24,8 +25,8 @@ def clean(df = pd.DataFrame) -> pd.DataFrame:
 @task(log_prints=True)
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out as a parquet file"""
-    #path = Path(f"/Users/krappo/data-engineering-zoomcamp/week2/data/{color}/{dataset_file}.parquet")
-    path = Path(f"../data/{color}/{dataset_file}.parquet")
+    path = Path(f"/Users/krappo/data-engineering-zoomcamp/week2/data/{color}/{dataset_file}.parquet")
+    #path = Path(f"../data/{color}/{dataset_file}.parquet")
     df.to_parquet(path, compression="gzip")
     return path
 
@@ -41,11 +42,8 @@ def write_gcs(path: Path) -> None:
 
 
 @flow(log_prints=True)
-def etl_web_to_gc() -> None:
+def etl_web_to_gc(year:int, month:int, color: str) -> None:
     """The main ETL function"""
-    color = "yellow"
-    year = "2021"
-    month = 1
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     print(dataset_file)
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
@@ -54,5 +52,18 @@ def etl_web_to_gc() -> None:
     path = write_local(df_clean, color, dataset_file)
     write_gcs(path)
 
+@flow()
+def etl_parent_flow (
+    months: list[int] = [1, 2],
+    year: int = 2021,
+    color: str = "yellow"
+):
+    for month in months:
+        etl_web_to_gc(year, month, color)
+
+
 if __name__ == "__main__":
-    etl_web_to_gc()
+    color = "yellow"
+    months = [1,2,3]
+    year = 2021
+    etl_parent_flow(months,year,color)
